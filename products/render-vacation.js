c5c1afd0e5f07d4ba72f0a2900a476985edfcc17
surelis segarra <sdarianne34@gmail.com>
2020-01-22 16:43:09 -0800

    added descriptions

--- products/render-vacation.js
+++ products/render-vacation.js
@@ -1,24 +1,30 @@
 function renderVacay(vacay) {
     const li = document.createElement('li');
     li.className = vacay.category;
-    li.title = vacay.description;
+    
 
     const h3 = document.createElement('h3');
     h3.textContent = vacay.name;
     li.appendChild(h3);
 
+    const descriptions = document.createElement('description');
+    descriptions.textContent = vacay.description;
+    li.appendChild(descriptions);
+    
 
     const img = document.createElement('img');
     img.src = vacay.image;
     img.alt = vacay.name + ' image';
     li.appendChild(img);
 
+
+
     const p = document.createElement('p');
     p.className = 'price';
-
     const usd = '$' + vacay.price.toFixed(2);
-
     p.textContent = usd;
+
+ 
     
     const button = document.createElement('button');
     button.textContent = 'Add';
